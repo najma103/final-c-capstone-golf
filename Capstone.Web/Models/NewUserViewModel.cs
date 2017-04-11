6f464c4c1643232636eb7db01c4efc2103103797
Evan Gilcrest <egilcrest1@gmail.com>
2017-04-11 09:21:14 -0400

    commit to pull

---
+++ Capstone.Web/Models/NewUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Capstone.Web.Models
+{
+    public class NewUserViewModel
+    {
+        [Required(ErrorMessage = "A username is required")]
+        [Display(Name = "User Name:")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Password:")]
+        [RegularExpression("^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Passwords do not match")]
+        [Display(Name = "Confirm Password:")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Capstone.Web/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class User
+    {
+        //Put all the columns here
+        //...
+
+
+    }
+}
