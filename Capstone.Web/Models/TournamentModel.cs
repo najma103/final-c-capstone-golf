a855a9c111a7241ef7d84875aa097975b20ac3b2
Najma Aden <najma.aden@gmail.com>
2017-04-11 03:50:21 -0500

    added create tournament view and controller

---
+++ Capstone.Web/Models/TournamentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class TournamentModel
+    {
+        [Required(ErrorMessage = "*")]
+        public string GameType { get; set; }
+        [Required(ErrorMessage = "*")]
+        public string MaxPlayers { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string GameStartDate { get; set; }
+    }
+}
