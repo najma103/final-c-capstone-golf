4c3e6e365b25c159196262d4bbe03ac45bdb53f6
Evan Gilcrest <egilcrest1@gmail.com>
2017-04-11 12:30:43 -0400

    added some properties with validation

--- Capstone.Web/Models/TournamentModel.cs
+++ Capstone.Web/Models/TournamentModel.cs
@@ -9,11 +9,14 @@ namespace Capstone.Web.Models
     public class TournamentModel
     {
         [Required(ErrorMessage = "*")]
+        [Display(Name = "Game Type", Prompt = "Checkers, Chess, etc.")]
         public string GameType { get; set; }
         [Required(ErrorMessage = "*")]
+        [Display(Name = "Max # of Players")]
         public string MaxPlayers { get; set; }
 
         [Required(ErrorMessage = "*")]
-        public string GameStartDate { get; set; }
+        [Display(Name = "Start Date")]
+        public DateTime GameStartDate { get; set; }
     }
 }
