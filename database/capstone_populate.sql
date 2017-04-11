fe648f0c5fefa6531bb80b2163ca78d86831f88b
Nick Collins <worksaul560@gmail.com>
2017-04-11 12:57:52 -0400

    finished user data

--- database/capstone_populate.sql
+++ database/capstone_populate.sql
@@ -1,456 +1,532 @@
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('awranglers',
 'awranglers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('bstallions',
 'bstallions@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('bbreakers',
 'bbreakers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('nolabreakers',
 'nolabreakers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('pbreakers',
 'pbreakers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('cblitz',
 'cblitz@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('dgold',
 'dgold@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('hgamblers',
 'hgamblers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('jbulls',
 'jbulls@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('laexpress',
 'laexpress@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('mshowboats',
 'mshowboats@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('mpanthers',
 'mpanthers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('njgenerals',
 'njgenerals@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('oinvaders',
 'oinvaders@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('ooutlaws',
 'ooutlaws@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('orenegades',
 'orenegades@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 )
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('pstars',
 'pstars@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('bstars',
 'bstars@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('pmaulers',
 'pmaulers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('sagunslingers',
 'sagunslingers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('tbandits',
 'tbandits@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('wfederals',
 'wfederals@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('dfelons',
 'dfelons@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('dlemons',
 'dlemons@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('lariots',
 'lariots@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('mdealers',
 'mdealers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('mbeers',
 'mbeers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('njinformants',
 'njinformants@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('raliens',
 'raliens@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('sadefenders',
 'sadefenders@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('sfferries',
 'sfferries@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('ajoes',
 'ajoes@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('cdonkeys',
 'cdonkeys@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('pcobras',
 'pcobras@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('akillz',
 'akillz@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('ljacks',
 'ljacks@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('tblitzkrieg',
 'tblitzkrieg@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('okamikazes',
 'okamikazes@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('lvpd',
 'lvpd@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('psfg',
 'psfg@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('tgg',
 'tgg@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('milfs',
 'milfs@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('mknucklers',
 'mknucklers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('nolocp',
 'nolocp@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('mmulchers',
 'mmulchers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('smullets',
 'smullets@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('t417',
 't417@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('wedgies',
 'wedgies@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('yetis',
 'yetis@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('pouncers',
 'pouncers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('smashers',
 'smashers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('ss300',
 'ss300@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('cchiefs',
 'cchiefs@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('liducks',
 'liducks@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('sbulldogs',
 'sbulldogs@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('ppatriots',
 'ppatriots@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('lgears',
 'lgears@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('hpresidents',
 'hpresidents@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('bcblades',
 'bcblades@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('kgougers',
 'kgougers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('mpigs',
 'mpigs@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('sice',
 'sice@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('hhighlanders',
 'hhighlanders@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('sjshamrocks',
 'sjshamrocks@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('qvictoires',
 'qvictoires@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('apatriots',
 'apatriots@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('oassassins',
 'oassassins@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('wwheelers',
 'wwheelers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('cminutemen',
 'cminutemen@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('hsteelers',
 'hsteelers@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('lkings',
 'lkings@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('mlords',
 'mlords@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('bblackjacks',
 'bblackjacks@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('mcorsairs',
 'mcorsairs@virtucon.net',
 'qwerty',
-'competitor'
+'competitor',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('scrane',
 'scrane@virtucon.net',
 'qwerty',
-'organizer'
+'organizer',
+'bat'
 );
-INSERT INTO [dbo].[users] ([username], [email], [password], [role]) VALUES
+INSERT INTO [dbo].[users] ([username], [email], [password], [role], [salt]) VALUES
 ('pheavensbee',
 'pheavensbee@virtucon.net',
 'qwerty',
-'organizer'
+'organizer',
+'bat'
 );
