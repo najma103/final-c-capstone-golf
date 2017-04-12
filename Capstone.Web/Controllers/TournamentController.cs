965fd5e29381a0137bb4b30bd5efca176fe6f8c0
Nick Collins <worksaul560@gmail.com>
2017-04-12 10:57:54 -0400

    created tourny controller and DAL method for single tourny

---
+++ Capstone.Web/Controllers/TournamentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone.Web.Controllers
+{
+    public class TournamentController : Controller
+    {
+        // GET: Tournament
+        public ActionResult Detail(int tournamentId)
+        {
+
+            return View();
+        }
+    }
+}
--- Capstone.Web/DAL/TournamentSqlDal.cs
+++ Capstone.Web/DAL/TournamentSqlDal.cs
@@ -11,6 +11,7 @@ namespace Capstone.Web.DAL
     {
         private string connectionString = @"Data Source=desktop-58f8ch1\sqlexpress;Initial Catalog=Capstone;Integrated Security=True";
 
+        private const string getATournamentSql = "SELECT * FROM tournaments WHERE tournament_id = @tournament_id";
         private const string getAllTournamentsSql = "SELECT * FROM tournaments order by tournament_name";
 
         public string ConnectionString
@@ -50,5 +51,42 @@ namespace Capstone.Web.DAL
                 throw exception;
             }
         }
+
+        public Tournament getATournament(int tournament_id)
+        {
+            Tournament tempTournament = new Tournament();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(getATournamentSql, conn);
+                    cmd.Parameters.AddWithValue("@tournament_id", tournament_id);
+
+                    SqlCommand command = new SqlCommand(getATournamentSql, conn);
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Tournament t = new Tournament();
+
+                        t.Id = Convert.ToInt32(reader["tournament_id"]);
+                        t.Name = Convert.ToString(reader["tournament_name"]);
+                        t.OrganizerId = Convert.ToInt32(reader["organizer_id"]);
+                        t.StartDate = Convert.ToDateTime(reader["start_date"]);
+                        t.EndDate = Convert.ToDateTime(reader["end_date"]);
+                        t.CompetitorLimit = Convert.ToInt32(reader["competitor_limit"]);
+
+                        tempTournament = t;
+                    }
+                }
+                return tempTournament;
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
     }
 }
